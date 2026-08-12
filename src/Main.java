5af83700850d88fe592c102b8bb656c25b957470
abhay-chandra22 <abhaychandra2004@gmail.com>
2026-08-13 01:55:14 +0530

    Add Student model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import model.Student;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Student Management System");
+        Student st = new Student(
+                101,
+                "Abhay Chandra",
+                "abhay@gmail.com",
+                22,
+                "CSE"
+        );
+        System.out.println(st);
     }
 }
