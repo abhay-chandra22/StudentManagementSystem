a779bb4ce72676527ecd67f47f2824f090bdaef8
abhay-chandra22 <abhaychandra2004@gmail.com>
2026-08-13 12:38:03 +0530

    Implement Student search with ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
 import model.Student;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        ArrayList<Student> students = new ArrayList<>();
         System.out.println("Student Management System");
-        Student st = new Student(
+        Student student1 = new Student(
                 101,
                 "Abhay Chandra",
                 "abhay@gmail.com",
                 22,
                 "CSE"
         );
-        System.out.println(st);
+        Student student2 = new Student(
+                102,
+                "Rahul",
+                "rahul@gmail.com",
+                21,
+                "ECE"
+        );
+        Student student3 = new Student(
+                103,
+                "Aadi",
+                "aadi@gmail.com",
+                20,
+                "CSE"
+        );
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+
+        for(Student student : students){
+            System.out.println(student);
+
+        }
+
+        System.out.println("Number of Students : " + students.size());
+        System.out.println("Third Student : " + students.get(2));
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Student ID: ");
+        int searchId = sc.nextInt();
+        boolean found = false;
+        System.out.println("Searching for student with ID " + searchId);
+        for (Student student : students) {
+            if (student.getId() == searchId) {
+                System.out.println("Student found: " + student);
+                found = true;
+            }
+        }
+        if(!found){
+            System.out.println("Student not found");
+        }
+
+
     }
 }
