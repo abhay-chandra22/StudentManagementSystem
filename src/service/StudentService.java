4a474e14744a2f0a1d7f6ea3ad094a0ea5b09df3
abhay-chandra22 <abhaychandra2004@gmail.com>
2026-08-19 15:26:50 +0530

    Refactor student validation

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -14,24 +14,7 @@ public class StudentService {
         return studentDAO.getAllStudents();
     }
     public void addStudent(Student student) throws SQLException{
-        if(student.getId() < 1){
-            System.out.println("Invalid Student ID");
-            return;
-        }
-        if(student.getName().isBlank()){
-            System.out.println("Invalid Student Name");
-            return;
-        }
-        if (!student.getEmail().contains("@") || !student.getEmail().contains(".")) {
-            System.out.println("Invalid Email");
-            return;
-        }
-        if(student.getAge() < 1 || student.getAge() > 100){
-            System.out.println("Invalid Age");
-            return;
-        }
-        if (student.getCourse().isBlank()) {
-            System.out.println("Invalid Course");
+        if(!validateStudent(student)){
             return;
         }
         if(findStudentById(student.getId()) == null){
@@ -42,6 +25,9 @@ public class StudentService {
     }
 
     public void updateStudent(Student student) throws SQLException{
+        if(!validateStudent(student)){
+            return;
+        }
         if(findStudentById(student.getId()) != null) {
             studentDAO.updateStudent(student);
         }else{
@@ -55,5 +41,28 @@ public class StudentService {
         }else{
             System.out.println("Student doesn't exist");
         }
+    }
+    private boolean validateStudent(Student student){
+        if(student.getId() < 1){
+            System.out.println("Invalid Student ID");
+            return false;
+        }
+        if(student.getName().isBlank()){
+            System.out.println("Invalid Student Name");
+            return false;
+        }
+        if (!student.getEmail().contains("@") || !student.getEmail().contains(".")) {
+            System.out.println("Invalid Email");
+            return false;
+        }
+        if(student.getAge() < 1 || student.getAge() > 100){
+            System.out.println("Invalid Age");
+            return false;
+        }
+        if (student.getCourse().isBlank()) {
+            System.out.println("Invalid Course");
+            return false;
+        }
+        return true;
     }
 }
