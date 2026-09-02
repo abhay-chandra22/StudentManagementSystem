8c9b6b8212d78a9373eb3d39a71c5f8c6594da00
abhay-chandra22 <abhaychandra2004@gmail.com>
2026-09-03 01:51:00 +0530

    Return proper HTTP statues for POST students

--- src/com/studentmanagement/controller/StudentController.java
+++ src/com/studentmanagement/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.studentmanagement.controller;
 
+import com.studentmanagement.service.OperationStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.studentmanagement.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,11 @@ import com.studentmanagement.model.Student;
 import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.studentmanagement.service.StudentOperationResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import com.studentmanagement.service.OperationStatus;
 
 @RestController
 public class StudentController {
@@ -30,5 +37,18 @@ public class StudentController {
         }else{
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<StudentOperationResult> addStudent(@RequestBody Student student) throws StudentManagementException{
+        StudentOperationResult result = studentService.addStudent(student);
+        if(result.getStatus() == OperationStatus.SUCCESS){
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        }else if(result.getStatus() == OperationStatus.INVALID_DATA){
+            return ResponseEntity.badRequest().body(result);
+        }else if(result.getStatus() == OperationStatus.DUPLICATE_ID){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(result);
+        }
+        return ResponseEntity.badRequest().body(result);
     }
 }
