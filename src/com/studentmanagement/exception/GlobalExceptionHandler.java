3960788a737a7c49a954f4e5f840cb2f9dd93ac1
abhay-chandra22 <abhaychandra2004@gmail.com>
2026-09-07 01:46:33 +0530

    Handle request body errors globally

--- src/com/studentmanagement/exception/GlobalExceptionHandler.java
+++ src/com/studentmanagement/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.studentmanagement.exception;
 
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +14,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleStudentManagementException(StudentManagementException e){
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value() , e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException e){
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value() , "Invalid request body");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
